31dc46c0447d2e5096725765fc6358fe000dafaa
Rudrakshi soni <sonirudrakshi99@gmail.com>
2021-04-24 13:20:38 +0530

    added postgresql

--- PizzaBackend/settings.py
+++ PizzaBackend/settings.py
@@ -80,13 +80,16 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PizzaApi',
+        'USER': 'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
